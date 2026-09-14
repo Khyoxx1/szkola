bcf7214d79fe7f61a486466ada38a6e8a153f681
Khyoxx <77795860+Khyoxx1@users.noreply.github.com>
2026-09-14 08:19:23 +0200

    Add files via upload

---
+++ Untitled-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$a = 1; 
+
+echo $a . "\n";                                      
+echo -$a . "\n";                                    
+echo ($a + 3) . "\n";                               
+echo ($a - 5) . "\n";                                
+echo ($a * 2) . "\n";                                
+echo ($a / 5) . "\n";                                
+echo ($a % 2) . "\n";                                
+echo pow($a, 3) . "\n";                              
+echo "Wartość zmiennej \$a wynosi: " . $a . " i już się nie zmieni.\n"; 
+?>
+</body>
+</html>
